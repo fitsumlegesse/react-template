08acd1010985d7c011e35b1879cd8097934613ee
fitsumlegesse@icloud.com <fitsumlegesse@gmail.com>
2022-08-18 21:44:58 -0700

    modifies footer

--- src/components/FooterComponent/FooterComponent.js
+++ src/components/FooterComponent/FooterComponent.js
@@ -1,19 +1,24 @@
 import React from 'react'
-import './FooterComponent.css'
 import fb from '../../img/fb-icon.png'
 import ln from '../../img/ln-icon.png'
 import tw from '../../img/tw-icon.png'
 import sc from '../../img/sc-icon.png'
+import './FooterComponent.css'
 
 const SectionTwo = () => {
     return (
         <div className='footer-container'>
-            
              <div className='social-icon'>
                 <img className='icons' src={fb} alt='hh'/>
                 <img className='icons' src={ln} alt='hh'/>
                 <img className='icons' src={tw} alt='hh'/>
                 <img className='icons' src={sc} alt='hh'/>
+            </div>
+            <div className='footer-text'>
+                <h1>Hello</h1>
+                <p>Hellosafdadsfdsa asfsasdfdasf fsadf dsfasd ftl </p>
+                <p>Hellosafdadsfdsa asfsasdfdasf fsadf dsfasd ftl </p>
+                <p>Hellosafdadsfdsa asfsasdfdasf fsadf dsfasd ftl </p>
             </div>
                 
         </div>
